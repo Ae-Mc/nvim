f04dc0006074d3931e728d2a578f961a72c385cc
Ae-Mc <ae_mc@mail.ru>
2022-01-17 21:59:19 +0300

    Try to install lsp plugins only when lspconfig, nvim-lsp-installer and nvim-cmp plugins installed

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -6,53 +6,72 @@
 -- nvim-lsp-installer repo
 local servers = {'sumneko_lua'}
 
-local lsp_installer = require('nvim-lsp-installer')
-local lspconfig = require('plugins.lspconfig')
-
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
-)
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
 local language_server_options_modifiers = {
     ['sumneko_lua'] = function(opts)
-	opts.settings = {
-	    Lua = {
-		runtime = { version = 'LuaJIT', path = runtime_path },
-		diagnostics = { globals = {'vim'} },
-		workspace = {
-		    library = vim.api.nvim_get_runtime_file("", true),
-		},
-		telemetry = { enabled = false },
-	    }
-	}
+    opts.settings = {
+        Lua = {
+            runtime = { version = 'LuaJIT', path = runtime_path },
+            diagnostics = { globals = {'vim'} },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true)
+            },
+            telemetry = { enabled = false },
+        }
+    }
     end,
 }
 
--- Create on_server_ready event listener
-lsp_installer.on_server_ready(function (server)
-    local options = {
-	on_attach = lspconfig.on_attach,
-	flags = { debounce_text_changes = 150 },
-	capabilities = capabilities,
-    }
+local plugins = require('config.plugins-list')
+local main, test_plugins
 
-    -- If there is any option modifiers for current language server than apply
-    -- them
-    if language_server_options_modifiers[server.name] then
-	language_server_options_modifiers[server.name](options)
-    end
+function test_plugins()
+    if (
+        plugins['nvim-lsp-installer']
+        and plugins['lspconfig']
+        and plugins['nvim-cmp']
+    ) then
+    main()
+end
+end
 
-    server:setup(options)
-end)
+function main()
+    local lsp_installer = require('nvim-lsp-installer')
+    local cmp_nvim_lsp = require('cmp_nvim_lsp')
+    local lspconfig = require('plugins.lspconfig')
 
--- Install all servers
-for _, lsp in ipairs(servers) do
-    local server_is_found, server = lsp_installer.get_server(lsp)
+    local capabilities = cmp_nvim_lsp.update_capabilities(
+        vim.lsp.protocol.make_client_capabilities()
+    )
 
-    if server_is_found and not server:is_installed() then
-	server:install()
+    -- Create on_server_ready event listener
+    lsp_installer.on_server_ready(function (server)
+        local options = {
+            on_attach = lspconfig.on_attach,
+            flags = { debounce_text_changes = 150 },
+            capabilities = capabilities,
+        }
+
+        -- If there is any option modifiers for current language server than
+        -- apply them
+        if language_server_options_modifiers[server.name] then
+            language_server_options_modifiers[server.name](options)
+        end
+
+        server:setup(options)
+    end)
+
+    -- Install all servers
+    for _, lsp in ipairs(servers) do
+        local server_is_found, server = lsp_installer.get_server(lsp)
+
+        if server_is_found and not server:is_installed() then
+            server:install()
+        end
     end
 end
+
+test_plugins()
