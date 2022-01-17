e37758ca1599cc62566a7551bb4e7c420041655d
Alexandr Makurin <ae_mc@mail.ru>
2022-01-18 00:20:38 +0300

    Add C-Tab and C-S-Tab shortcuts for buffer switching
    Create prepare_opts and all_map util functions

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,10 +2,25 @@
 -- Модуль с настройками сочетаний клавиш
 --------------------------------------------------------------------------------
 
-local function nmap(lhs, rhs, opts)
+local function prepare_opts(opts)
     opts = opts or {}
+    if not opts.noremap then
+        opts.noremap = true
+    end
+    return opts
+end
+
+local function nmap(lhs, rhs, opts)
+    opts = prepare_opts(opts)
     vim.api.nvim_set_keymap('n', lhs, rhs, opts)
+end
+local function all_map(lhs, rhs, opts)
+    opts = prepare_opts(opts)
+    vim.api.nvim_set_keymap('', lhs, rhs, opts)
+    vim.api.nvim_set_keymap('!', lhs, rhs, opts)
 end
 
 nmap(',<space>', ':nohlsearch<CR>')
 nmap('Y', 'yy')
+all_map('<C-Tab>', '<Cmd>:BufferLineCycleNext<CR>')
+all_map('<C-S-Tab>', '<Cmd>:BufferLineCyclePrev<CR>')
