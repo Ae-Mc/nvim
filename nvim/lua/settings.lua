f179a202d5f393ad3ae70fe73c05fa7b48bf0b44
Ae-Mc <ae_mc@mail.ru>
2022-01-17 22:39:49 +0300

    Add more configuration to settings module
    Add cursor restoration
    Add scrolloff
    Add tabstop
    Add list and listchars

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -5,4 +5,14 @@
 vim.opt.expandtab = true
 vim.opt.mouse = 'a'
 vim.opt.number = true
+vim.opt.scrolloff = 6
 vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.list = true  -- Enables listchars. Necessary for indent-blankline plugin
+vim.opt.listchars:append({trail = '●', tab = '>-'})
+
+-- Восстановление позиции курсора
+vim.cmd([[ autocmd BufRead * autocmd FileType <buffer> ++once
+      \ if &ft !~# 'commit\|rebase' && line("'\"") > 1
+      \ && line("'\"") <= line("$") | exe 'normal! g`"' | endif
+]])
