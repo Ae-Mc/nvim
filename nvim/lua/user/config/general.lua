21629dbc11d9b5b9bc48a4b3d6312ba01f5f5016
Ae-Mc <ae_mc@mail.ru>
2022-01-20 00:47:32 +0300

    Enable undo history

--- nvim/lua/user/config/general.lua
+++ nvim/lua/user/config/general.lua
@@ -3,15 +3,17 @@
 --------------------------------------------------------------------------------
 
 vim.o.expandtab = true
+vim.o.list = true  -- Enables listchars. Necessary for indent-blankline plugin
+vim.opt.listchars:append({trail = '●', tab = '>-'})
 vim.o.mouse = 'a'
 vim.o.number = true
 vim.o.scrolloff = 6
 vim.o.shiftwidth = 4
+vim.o.smartindent = true
 vim.o.splitbelow = true
 vim.o.splitright = true
 vim.o.tabstop = 4
-vim.o.list = true  -- Enables listchars. Necessary for indent-blankline plugin
-vim.opt.listchars:append({trail = '●', tab = '>-'})
+vim.o.undofile = true
 
 -- Восстановление позиции курсора
 vim.cmd([[ autocmd BufRead * autocmd FileType <buffer> ++once
