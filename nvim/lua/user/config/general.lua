b6a8f8dae19e5dd7793bbc80f073d96669d69f25
Ae-Mc <ae_mc@mail.ru>
2022-01-21 23:42:36 +0300

    Add powershell terminal support (if windows then powershell)

--- nvim/lua/user/config/general.lua
+++ nvim/lua/user/config/general.lua
@@ -16,6 +16,14 @@ vim.o.tabstop = 4
 vim.o.undofile = true
 -- Reduce updatetime which affects CursorHold
 vim.o.updatetime = 0
+if vim.fn.has('win32') == 1 then
+    vim.o.shell = 'powershell'
+    vim.o.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+    vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    vim.o.shellquote=''
+    vim.o.shellxquote=''
+end
 
 -- Восстановление позиции курсора
 vim.cmd([[ autocmd BufRead * autocmd FileType <buffer> ++once
