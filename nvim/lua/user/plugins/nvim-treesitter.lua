d8ac48533ce2fe6c29cdaa5d331872f844528c28
Ae-Mc <ae_mc@mail.ru>
2022-01-21 05:32:15 +0300

    set treesitter parsers source to maintained instead of all

--- nvim/lua/user/plugins/nvim-treesitter.lua
+++ nvim/lua/user/plugins/nvim-treesitter.lua
@@ -6,7 +6,7 @@ end
 
 require'nvim-treesitter.configs'.setup {
     -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = "all",
+    ensure_installed = "maintained",
 
     -- Install languages synchronously (only applied to `ensure_installed`)
     -- sync_install = false,
