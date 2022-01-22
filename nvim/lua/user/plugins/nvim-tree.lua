0466efbbdca4a6c413826398e9f953bcbeddda49
Ae-Mc <ae_mc@mail.ru>
2022-01-22 03:28:48 +0300

    Enable changed filename highlighting in nvim-tree

--- nvim/lua/user/plugins/nvim-tree.lua
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -1,4 +1,5 @@
 vim.g.nvim_tree_group_empty = 1
+vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_highlight_opened_files = 3
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_window_picker_exclude = {
